9df33a25a5fc60f4170509728437a00a75910468
Parth Sali <parthsali04@gmail.com>
2023-06-04 15:29:16 +0530

    update CG Assignment-03

--- SEM-04/CG/Assignment-03/Necklace.cpp
+++ SEM-04/CG/Assignment-03/Necklace.cpp
@@ -53,10 +53,105 @@ void drawCircle(int centerX, int centerY, int radius, int colorOne=1, int colorT
 
 }
 
+void displayPoint(float x, float y) {
+    // glColor3f(1, 1, 0);
+    glBegin(GL_POINTS);
+    glVertex2f(x, y);
+    glEnd();
+
+}
+
+void drawLine(float x1, float y1, float x2, float y2, char line = 'A') {
+    float dx = x2 - x1;
+    float dy = y2 - y1;
+    float m = dy / dx;
+
+    float steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+
+    float xInc = dx / (float) steps;
+    float yInc = dy / (float) steps;
+
+    float x = x1;
+    float y = y1;
+
+    displayPoint(x, y);
+        for (int i = 0; i < steps; i++) {
+        
+            
+            if (line == 'B') {
+                if (i % 3 == 0) {
+                   displayPoint(x, y);
+                }
+            }
+            else if(line == 'C') {
+                if (i % 7 == 0) {
+                    displayPoint(x, y);
+                }
+            }
+            else {
+                displayPoint(x, y);
+            }
+            x += xInc;
+        y += yInc;
+        
+    }
+    
+    glFlush();
+
+}
+
 void display() {
 
     glClear(GL_COLOR_BUFFER_BIT);
+
+
     drawCircle(250, 100, 90);
+
+    glColor3f(1, 1, 0);
+    drawLine(250, 190, 197, 28, 'A');
+    glColor3f(1, 0, 1);
+    drawLine(250, 190, 303, 28, 'A');
+    glColor3f(0, 1, 1);
+    drawLine(164, 127, 334, 127, 'A');
+    glColor3f(1, 1, 0);
+    drawLine(164, 127, 303, 28, 'A');
+    glColor3f(1, 0, 1);
+    drawLine(334, 127, 197, 28, 'A');
+    glColor3f(0, 1, 1);
+
+    drawLine(250, 190, 164, 127, 'A');
+    glColor3f(1, 1, 0);
+    drawLine(197, 28, 164, 127, 'A');
+    glColor3f(1, 0, 1);
+    drawLine(197, 28, 303, 28, 'A');
+    glColor3f(1, 1, 0);
+    drawLine(303, 28, 334, 127, 'A');
+    glColor3f(0, 1, 1);
+    drawLine(334, 127, 250, 190, 'A');
+    glColor3f(1, 0, 1);
+
+    // drawLine(304, 28, 165, , 'B');
+    // glBegin(GL_LINES);
+    // glVertex2i(250, 190);
+    // glVertex2i(335, 28);
+
+
+    // glVertex2i(335, 28);
+    // glVertex2i(197, 173);
+
+    // glVertex2i(197, 173);
+    // glVertex2i(303, 173);
+
+
+    // glVertex2i(303, 173);
+    // glVertex2i(165, 28);
+
+    
+    // glVertex2i(165, 28);
+    //  glVertex2i(250, 190);
+    
+    // glEnd();
+
     drawCircle(150, 150, 70);
     drawCircle(350, 150, 70);
 
@@ -87,6 +182,8 @@ void display() {
     
     glFlush();
 }
+
+
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
